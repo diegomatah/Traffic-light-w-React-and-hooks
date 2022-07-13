82740b305fbd700925f576343fe72371ba1c4429
diegomatah <diegoa.matah@gmail.com>
2022-07-13 02:06:32 +0000

    tarea semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,19 +6,10 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="TrafficLight row d-flex-justify-content-between mx-3 mt-3" style={{"height":"300px","width":"80px","backgroundColor":"black","borderRadius":"15px"}}>
+			<div className="GreenLight mx-3" style={{"height":"50px","marginTop":"30px", "width":"50px","backgroundColor":"green","borderRadius":"25px"}}></div>
+			<div className="YellowLight mx-3" style={{"height":"50px","width":"50px","backgroundColor":"yellow","borderRadius":"25px"}}></div>
+			<div className="RedLight mx-3"style={{"height":"50px","width":"50px","backgroundColor":"red","borderRadius":"25px"}}></div>
 		</div>
 	);
 };
